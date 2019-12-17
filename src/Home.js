0a989012157c95019d2ecf6255663dca53f0b2ee
njeririziki <rizikinjeri@gmail.com>
2019-12-17 23:39:17 +0300

    Updated ui

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import { makeStyles} from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button';
 import Appbar from '@material-ui/core/Appbar';
 import Toolbar from '@material-ui/core/Toolbar';
-import InputBase from '@material-ui/core/InputBase';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -11,16 +10,33 @@ import ListItemText from '@material-ui/core/ListItemText';
 import * as Icon from 'react-feather';
 import Drawer from '@material-ui/core/Drawer';
 import Divider from '@material-ui/core/Divider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton'
 
-const drawerWidth= 280;
+const drawerWidth= 240;
 const useStyles= makeStyles(theme=>({
   root:{
     display:'flex'
   },
 
- 
+ appbar:{
+  boxShadow:'none',
+  minHeight:80,
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  
+ },
+ appbarShift:{
+  width : `calc (100% - ${drawerWidth}px)`,
+  marginLeft:-drawerWidth,
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.easeOut,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+ },
   toolbar: {
-    minHeight:60,
    display:'flex',
    flexDirection:'row',
    justifyContent:'space-between',
@@ -31,12 +47,31 @@ const useStyles= makeStyles(theme=>({
   },
   drawer:{
    width: drawerWidth,
+   backgroundColor:'#b9f6ca',
+   flexShrink:0
+  },
+  drawerPaper:{
+    width: drawerWidth,
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
     marginLeft : - drawerWidth,
-    backgroundColor:'#454545'
+    backgroundColor:'#e1f5fe',
+    minHeight: 85,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    
+    
+  },
+  contentShift:{
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+    backgroundColor:'#F2F2F2'
   }
 })
 
@@ -47,35 +82,63 @@ const useStyles= makeStyles(theme=>({
     function drawerOpen(){
        setOpen(true)
     }
+    function drawerClose(){
+      setOpen(false)
+   }
 
     return(
     <div className={classes.root}>
-      <div>
+      
+      <CssBaseline/>
       <Appbar 
-      style ={{backgroundColor:'#349D63'}}>
+      style ={{backgroundColor:'#00bfa5'}}
+      className={classes.appbar}>
         <Toolbar className={classes.toolbar}>
           <Icon.Menu
           onClick={drawerOpen}
           />
           <Typography
-          style={{fontSize:30}}>
+          style={{fontSize:50, fontFamily:'Julius Sans One',
+           color:'#000000',fontWeight:20}}
+           >
             Finance Portal
           </Typography>
-          <Icon.Search/>
-          <InputBase 
-          placeholder ="Search"
-          >
-          </InputBase>
+         
+         <Button
+         variant='contained'
+         style ={{backgroundColor:'#b0bec5',fontSize:15, 
+         fontFamily:'Julius Sans One',
+           color:'#000000',fontWeight:20}}
+           >
+         >
+           Log out
+           </Button>
+         
         </Toolbar>
       </Appbar>
-      </div>
-
-    <Drawer
+      
+   <div>
+   <Drawer
     open={open}
     variant='persistent'
     anchor ='left'
     className={classes.drawer}
+    classes={{
+      paper: classes.drawerPaper,
+    }}
+
     >
+      <IconButton
+      onClick={drawerClose}
+      style={{display:'flex',justifyContent:'flex-end'}}>
+        <Icon.ChevronLeft/>
+      </IconButton>
+      <Typography
+          style={{fontSize:30, fontFamily:'Martel',
+           color:'#000000',fontWeight:20}}
+           >  
+          </Typography>
+     <Divider/>
      <List>
     {['Tuition Balance','Pocket Money','Cocurriculars'].map((text)=>(
       <ListItem
@@ -90,10 +153,26 @@ const useStyles= makeStyles(theme=>({
     ))}
      </List>  
     </Drawer>
+   </div>
+   
     <main className={classes.content}>
-    <div >
-    <Typography>
-      lorem ipsum
+    <div 
+    style={{
+      display:'flex',
+      minHeight:'100vh',
+      flexDirection:'row',
+      alignContent:'center',
+      justifyContent:'center',
+      backgroundColor:'#e1f5fe',
+      marginLeft:-drawerWidth
+    }}>
+    <Typography
+   align='center'
+    style={{alignSelf:'center', fontSize:30, fontFamily:'Martel',
+    color:'#000000',fontWeight:20}}
+    >
+      Student Name : Njeri <br/>
+      Student Id:01682
     </Typography>
     </div> 
     </main>
